b609cb89313c28077d72d5ba159ec0cac7d06309
cappie013 <besnard.nicolas@gmail.com>
2013-01-28 23:45:15 +0100

    - GNOME-TERMINAL tomorrow themes

--- conf.sh
+++ conf.sh
@@ -3,18 +3,19 @@
 echo "- Emacs"
 cp EMACS/.emacs ~/
 cp EMACS/.myemacs ~/
-cp EMACS/-r .emacs.d/ ~/
+cp -rf EMACS/.emacs.d/ ~/
 echo "- Conky"
-sudo cp CONKY/.conkyrc ~/
+cp CONKY/.conkyrc ~/
 sudo apt-get update
-sudo apt-get install valgrind htop emacs conky
-sudo apt-get install compiz compiz-core compiz-gnome compiz-plugins compiz-plugins-default compiz-plugins-extra compiz-plugins-main compiz-plugins-main-default compizconfig-backend-gconf compizconfig-settings-manager libcompizconfig0 libdecoration0 python-compizconfig fusion-icon gnome-tweak-tool
+sudo apt-get install valgrind htop conky
+sudo apt-get install compizconfig-settings-manager
+#sudo apt-get install compiz compiz-core compiz-gnome compiz-plugins compiz-plugins-default compiz-plugins-extra compiz-plugins-main compiz-plugins-main-default compizconfig-backend-gconf compizconfig-settings-manager libcompizconfig0 libdecoration0 python-compizconfig fusion-icon gnome-tweak-tool
 echo "- ZSH"
-sudu apt-get install git ssh zsh
+sudo apt-get install ssh zsh
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 ssh-keygen
 
-sudo cp XUBUNTU/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+#sudo cp XUBUNTU/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 chsh -s /bin/zsh
 
 git config --global user.name "cappie013"

772efaba0aad7d5afadd9974b773ec7461cc3457
cappie013 <besnard.nicolas@gmail.com>
2013-05-31 06:21:43 -0700

    FIX allegro bug

--- allegro5.sh
+++ allegro5.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get update
 sudo apt-get install build-essential git cmake xorg-dev libgl1-mesa-dev libglu-dev
-sudo apt-get install libpng-dev libcurl4-dev libfreetype6-dev libjpeg-dev libvorbis-dev libopenal-dev libphysfs-dev libgtk2.0-dev libasound-dev libpulse-dev libflac-dev libdumb1-dev
+#sudo apt-get install libpng-dev libcurl4-dev libfreetype6-dev libjpeg-dev libvorbis-dev libopenal-dev libphysfs-dev libgtk2.0-dev libasound-dev libpulse-dev libflac-dev libdumb1-dev
 git clone git://git.code.sf.net/p/alleg/allegro
 cd allegro
 git checkout 5.0

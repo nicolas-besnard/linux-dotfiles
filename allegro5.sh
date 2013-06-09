20c4ed01f8178155a76e1bf6963eac895fe9c980
cappie013 <besnard.nicolas@gmail.com>
2013-06-09 22:15:21 +0200

    - New Snippet

--- allegro5.sh
+++ allegro5.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get install build-essential git cmake xorg-dev libgl1-mesa-dev libglu-dev
+# sudo apt-get update
+# sudo apt-get install build-essential git cmake xorg-dev libgl1-mesa-dev libglu-dev
 #sudo apt-get install libpng-dev libcurl4-dev libfreetype6-dev libjpeg-dev libvorbis-dev libopenal-dev libphysfs-dev libgtk2.0-dev libasound-dev libpulse-dev libflac-dev libdumb1-dev
-git clone git://git.code.sf.net/p/alleg/allegro
+# git clone git://git.code.sf.net/p/alleg/allegro
+# cd allegro
+# git checkout 5.0
+# mkdir build
 cd allegro
-git checkout 5.0
-mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr ..
-make
+cd build
+sudo cmake -DCMAKE_INSTALL_PREFIX=/usr ..
+sudo make
 sudo make install

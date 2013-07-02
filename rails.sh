0490c4fd420a6ab719e31a8cc5f099a35dda4f70
cappie013 <besnard.nicolas@gmail.com>
2013-07-02 14:33:57 -0700

    change on rails script

--- rails.sh
+++ rails.sh
@@ -1,9 +1,3 @@
 sudo apt-get install curl &&
-curl -L https://get.rvm.io | bash -s stable &&
-source ~/.rvm/scripts/rvm &&
-rvm autolibs enable &&
-rvm requirements &&
-rvm install 1.9.3 &&
-rvm use 1.9.3 --default &&
-rvm rubygems current &&
+curl -L https://get.rvm.io | bash -s stable --ruby &&
 gem install rails

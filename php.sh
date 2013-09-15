28c699e6c98b1b71e126824fdc68796bbd2362dd
cappie013 <besnard.nicolas@gmail.com>
2013-09-15 07:31:39 -0700

    update

--- php.sh
+++ php.sh
@@ -1,8 +1,8 @@
 sudo apt-get install apache2 -y
 sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql -y
+sudo apt-get install dnsmasq -y
 sudo mysql_install_db
 sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt -y
-apt-get install dnsmasq -y
 sudo a2enmod vhost_alias
 sudo apt-get install phpmyadmin -y
 sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf.d/phpmyadmin.conf
